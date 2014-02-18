bba5065d6f7b1b55858ddeefaf4a34d62fa836cd
Hauke Schade <cnlpete@cnlpete.de>
2014-02-18 20:33:41 +0100

    going for sailfish :)

---
+++ harbour-sailbackground.pro
@@ -0,0 +1,49 @@
+# The name of your app.
+# NOTICE: name defined in TARGET has a corresponding QML filename.
+#         If name defined in TARGET is changed, following needs to be
+#         done to match new name:
+#         - corresponding QML filename must be changed
+#         - desktop icon filename must be changed
+#         - desktop filename must be changed
+#         - icon definition filename in desktop file must be changed
+
+VERSION = 0.0.3
+DEFINES += APP_VERSION=\\\"$$VERSION\\\"
+DEFINES += WALLPATH=\\\"/home/user/.config/meebackground_wallpaper.png\\\"
+DEFINES += GCONFKEY=\\\"/desktop/meego/background/portrait/picture_filename\\\"
+
+DEFINES += Q_OS_SAILFISH
+
+TARGET = harbour-sailbackground
+
+##CONFIG += sailfishapp
+
+QT += quick qml
+
+target.path = /usr/bin
+INSTALLS += target
+
+CONFIG += link_pkgconfig
+PKGCONFIG += sailfishapp
+INCLUDEPATH += /usr/include/sailfishapp
+
+qml_1.files = qml/MeeBackground/Sailfish
+qml_1.path = $$INSTALL_ROOT/usr/share/$$TARGET/qml
+qml_2.files = qml/MeeBackground/Models
+qml_2.path = $$INSTALL_ROOT/usr/share/$$TARGET/qml
+INSTALLS += qml_1 qml_2
+
+icon.files = images/$${TARGET}.png
+icon.path = /usr/share/icons/hicolor/86x86/apps
+INSTALLS += icon
+
+desktop.files = harbour-sailbackground.desktop
+desktop.path = /usr/share/applications
+INSTALLS += desktop
+
+SOURCES += main.cpp
+
+OTHER_FILES += rpm/harbour-sailbackground.spec \
+    rpm/harbour-sailbackground.yaml \
+    $$files(rpm/*)
+
--- main.cpp
+++ main.cpp
@@ -9,32 +9,61 @@
 //You should have received a copy of the GNU General Public License along with MeeBackground (on a Maemo/Meego system there is a copy
 //in /usr/share/common-licenses. If not, see http://www.gnu.org/licenses/.
 
-#include <QtGui/QApplication>
-#include <QtDeclarative/QDeclarativeContext>
-#include "qmlapplicationviewer.h"
+#if defined(Q_OS_SAILFISH)
+    #include <QGuiApplication>
+    #include <sailfishapp.h>
+    #ifdef QT_QML_DEBUG
+        #include <QtQuick>
+    #endif
+#else
+    #include <QtGui/QApplication>
+    #include <QDeclarativeContext>
+    #include "qmlapplicationviewer.h"
+#endif
+
 #include <QDir>
 
 #include "qmlutils.hh"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
+#if defined(Q_OS_SAILFISH)
+    QGuiApplication *app = SailfishApp::application(argc, argv);
+#else
     QScopedPointer<QApplication> app(createApplication(argc, argv));
+#endif
 
     app->setApplicationVersion(APP_VERSION);
     app->setApplicationName("MeeBackground");
     app->setOrganizationName("Hauke Schade");
 
-    QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationLockPortrait);
-    viewer.rootContext()->setContextProperty("QMLUtils", QMLUtils::instance());
-    viewer.rootContext()->setContextProperty("APP_VERSION", APP_VERSION);
+#if defined(Q_OS_SAILFISH)
+    QQuickView* viewer = SailfishApp::createView();
+#else
+    QmlApplicationViewer *viewer = new QmlApplicationViewer();
+    viewer->setOrientation(QmlApplicationViewer::ScreenOrientationLockPortrait);
+    viewer->rootContext()->setContextProperty("QMLUtils", QMLUtils::instance());
+#endif
+
+    viewer->rootContext()->setContextProperty("APP_VERSION", APP_VERSION);
 
     if (!QDir("/home/user/MyDocs/MeeBackground/").exists())
         QDir().mkdir("/home/user/MyDocs/MeeBackground/");
-    viewer.rootContext()->setContextProperty("PATH", "/home/user/MyDocs/MeeBackground/");
+    viewer->rootContext()->setContextProperty("PATH", "/home/user/MyDocs/MeeBackground/");
 
-    viewer.setMainQmlFile(QLatin1String("qml/Harmattan/main.qml"));
-    viewer.showExpanded();
+#ifdef Q_OS_HARMATTAN
+    viewer->setMainQmlFile(QLatin1String("qml/Harmattan/main.qml"));
+#elif defined(Q_OS_SAILFISH)
+    viewer->setSource(SailfishApp::pathTo("qml/Sailfish/harbour-sailbackground.qml"));
+#endif
+
+#ifdef Q_OS_HARMATTAN
+    viewer->showExpanded();
+#elif defined(Q_OS_SAILFISH)
+    viewer->show();
+#else
+    viewer->showExpanded();
+#endif
 
     return app->exec();
 }
